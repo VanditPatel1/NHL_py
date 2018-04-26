3f1c628bcfaabc1c76a4feaa02183277ea2f3aac
VanditPatel1 <patel.vandit1@gmail.com>
2018-04-25 23:00:26 -0600

    Added in SQL for new Table

--- create_tables.py
+++ create_tables.py
@@ -3,11 +3,33 @@ player_meta = \
 """
 CREATE TABLE player_meta (
     ID INT NOT NULL,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    team VARCHAR(20),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    team VARCHAR(40),
     number INT,
     position VARCHAR(20),
     PRIMARY KEY (ID)
+)
+"""
+
+shot_table = \
+"""
+CREATE TABLE shots (
+    ID INT NOT NULL,
+    PLAYER_ID INT NOT NULL,
+    x_coord INT,
+    y_coord INT,
+    game VARCHAR(30),
+    date DATETIME
+    period INT,
+    time VARCHAR(20),
+    goalie VARCHAR(50),
+    type VARCHAR(30),
+    goal BOOLEAN,
+    strength VARCHAR(20),
+    assist_1 VARCHAR(50),
+    assist_2 VARCHAR(50),
+    game_winning BOOLEAN,
+    empty_net BOOLEAN
 )
 """
