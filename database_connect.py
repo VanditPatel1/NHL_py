70ac43826718be0eb0e0473e499c73fbc5d49a4d
VanditPatel1 <patel.vandit1@gmail.com>
2018-04-25 23:00:51 -0600

    Refactored module to connect to DB

--- database_connect.py
+++ database_connect.py
@@ -1,33 +1,47 @@
 from config import DRIVER, SERVER, DATABASE, UID, PWD
 import psycopg2
-from create_tables import player_meta
+from create_tables import player_meta, shot_table
 
 
-def create_table():
-    conn = psycopg2.connect(host=SERVER,
-                            database=DATABASE,
-                             port=5432,
-                             user=UID,
-                             password=PWD)
+class db_connect():
 
+    def __init__(self):
+        try:
+            self.conn = psycopg2.connect(host=SERVER,
+                                    database=DATABASE,
+                                    port=5432,
+                                    user=UID,
+                                    password=PWD)
+            print ("Connection made to db...")
 
-    cur = conn.cursor()
-    cur.execute(player_meta)
-    conn.commit()
-    cur.close()
+        except:
+            print ("Cannot connect to db...")
+            raise
 
-def player_add(packet):
-    conn = psycopg2.connect(host=SERVER,
-                            database=DATABASE,
-                             port=5432,
-                             user=UID,
-                             password=PWD)
+    def create_table(self, new_table):
+        try:
+            cur = self.conn.cursor()
+            cur.execute(new_table)
 
+        except:
+            print ("Could not create table...")
+            raise
 
-    cur = conn.cursor()
-    cur.execute("""INSERT INTO player_meta VALUES {} on conflict do nothing""".format(tuple(packet)))
-    conn.commit()
-    cur.close()
+        else:
+            conn.commit()
+            cur.close()
+
+    def add_row(self, packet, table):
+        try:
+            cur = self.conn.cursor()
+            cur.execute("""INSERT INTO {0} VALUES {1} on conflict do nothing""".format(table, tuple(packet)))
+
+        except:
+            print ("Could not add to table {}, values {}").format(table, packet)
+
+        else:
+            conn.commit()
+            cur.close()
 
 
 if __name__ == '__main__':
