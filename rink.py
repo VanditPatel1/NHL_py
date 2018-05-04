2449e411cf82b29845ea1c10a5f3aa5672982bdb
VanditPatel1 <patel.vandit1@gmail.com>
2018-05-04 17:19:56 -0400

    rink features updated

--- rink.py
+++ rink.py
@@ -1,8 +1,6 @@
 
 
-
-
-rink_shape = list()
+rink_shapes = list()
 
 
 outer_rect = dict(
@@ -17,7 +15,7 @@ outer_rect = dict(
                 width=1,
             )
 )
-
+rink_shapes.append(outer_rect)
 
 outer_line = dict(
             type='line',
@@ -31,7 +29,7 @@ outer_line = dict(
                 width=1,
             )
 )
-
+rink_shapes.append(outer_line)
 
 outer_arc1 = dict(
             type='path',
@@ -42,7 +40,18 @@ outer_arc1 = dict(
                 width=1,
             )
 )
+rink_shapes.append(outer_arc1)
 
+outer_arc2 = dict(
+            type='path',
+            xref='x',
+            yref='y',
+            path='M -200 580 C -217 574, -247 532, -250 516.2',
+            line=dict(
+                width=1,
+            )
+)
+rink_shapes.append(outer_arc2)
 
 center_red_line = dict(
             type='line',
@@ -57,7 +66,7 @@ center_red_line = dict(
                 color='rgba(255, 0, 0, 1)'
             )
 )
-
+rink_shapes.append(center_red_line)
 
 end_line = dict(
             type='line',
@@ -72,7 +81,7 @@ end_line = dict(
                 color='rgba(255, 0, 0, 1)'
             )
 )
-
+rink_shapes.append(end_line)
 
 blue_line_shape = dict(
             type='rect',
@@ -81,14 +90,14 @@ blue_line_shape = dict(
             x0='250',
             y0='150.8',
             x1='-250',
-            y1='-145',
+            y1='155', #'-145',
             line=dict(
                 color='rgba(0, 0, 255, 1)',
                 width=1
             ),
             fillcolor='rgba(0, 0, 255, 1)'
 )
-
+rink_shapes.append(blue_line_shape)
 
 center_blue_spot = dict(
             type='circle',
@@ -104,9 +113,9 @@ center_blue_spot = dict(
             ),
             fillcolor='rgba(0, 0, 255, 1)'
 )
+rink_shapes.append(center_blue_spot)
 
-
-red_spot1 = dict(
+red_spot1_top = dict(
             type='circle',
             xref='x',
             yref='y',
@@ -120,7 +129,23 @@ red_spot1 = dict(
             ),
             fillcolor='rgba(255, 0, 0, 1)'
 )
+rink_shapes.append(red_spot1_top)
 
+red_spot2_top = dict(
+            type='circle',
+            xref='x',
+            yref='y',
+            x0='-135.5',
+            y0='121.8',
+            x1='-123.5',
+            y1='110.2',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            ),
+            fillcolor='rgba(255, 0, 0, 1)'
+)
+rink_shapes.append(red_spot2_top)
 
 red_spot1_circle = dict(
             type='circle',
@@ -135,9 +160,24 @@ red_spot1_circle = dict(
                 color='rgba(255, 0, 0, 1)'
             )
 )
+rink_shapes.append(red_spot1_circle)
 
+red_spot2_circle = dict(
+            type='circle',
+            xref='x',
+            yref='y',
+            x0='-217.6',
+            y0='487.2',
+            x1='-41.2',
+            y1='313.2',
+            line=dict(
+                width=1,
+                color='rgba(255, 0, 0, 1)'
+            )
+)
+rink_shapes.append(red_spot2_circle)
 
-parallel_line1 = dict(
+parallel_line1_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -150,9 +190,9 @@ parallel_line1 = dict(
                 width=1
             )
 )
+rink_shapes.append(parallel_line1_right)
 
-
-parallel_line2 = dict(
+parallel_line2_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -165,9 +205,99 @@ parallel_line2 = dict(
                 width=1
             )
 )
+rink_shapes.append(parallel_line2_right)
 
+parallel_line3_right = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='28.8',
+            y0='416.4',
+            x1='41',
+            y1='416.4',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line3_right)
 
-faceoff_line1 = dict(
+parallel_line4_right = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='28.8',
+            y0='384',
+            x1='41',
+            y1='384',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line4_right)
+
+parallel_line1_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-230',
+            y0='416.4',
+            x1='-217.8',
+            y1='416.4',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line1_left)
+
+parallel_line2_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-230',
+            y0='384',
+            x1='-217.8',
+            y1='384',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line2_left)
+
+parallel_line3_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-28.8',
+            y0='416.4',
+            x1='-41',
+            y1='416.4',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line3_left)
+
+parallel_line4_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-28.8',
+            y0='384',
+            x1='-41',
+            y1='384',
+            line=dict(
+                color='rgba(255, 0, 0, 1)',
+                width=1
+            )
+)
+rink_shapes.append(parallel_line4_left)
+
+faceoff_line1_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -180,9 +310,9 @@ faceoff_line1 = dict(
                 width=1
             )
 )
+rink_shapes.append(faceoff_line1_right)
 
-
-faceoff_line2 = dict(
+faceoff_line2_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -195,9 +325,9 @@ faceoff_line2 = dict(
                 width=1
             )
 )
+rink_shapes.append(faceoff_line2_right)
 
-
-faceoff_line3 = dict(
+faceoff_line3_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -210,9 +340,9 @@ faceoff_line3 = dict(
                 width=1
             )
 )
+rink_shapes.append(faceoff_line3_right)
 
-
-faceoff_line4 = dict(
+faceoff_line4_right = dict(
             type='line',
             xref='x',
             yref='y',
@@ -225,9 +355,69 @@ faceoff_line4 = dict(
                 width=1
             )
 )
+rink_shapes.append(faceoff_line4_right)
 
+faceoff_line1_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-141.17',
+            y0='423.4',
+            x1='-141.17',
+            y1='377',
+            line=dict(
+                color='rgba(10, 10, 100, 1)',
+                width=1
+            )
+)
+rink_shapes.append(faceoff_line1_left)
 
-goal_line1 = dict(
+faceoff_line2_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-117.62',
+            y0='423.4',
+            x1='-117.62',
+            y1='377',
+            line=dict(
+                color='rgba(10, 10, 100, 1)',
+                width=1
+            )
+)
+rink_shapes.append(faceoff_line2_left)
+
+faceoff_line3_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-153',
+            y0='406',
+            x1='-105.8',
+            y1='406',
+            line=dict(
+                color='rgba(10, 10, 100, 1)',
+                width=1
+            )
+)
+rink_shapes.append(faceoff_line3_left)
+
+faceoff_line4_left = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-153',
+            y0='394.4',
+            x1='-105.8',
+            y1='394.4',
+            line=dict(
+                color='rgba(10, 10, 100, 1)',
+                width=1
+            )
+)
+rink_shapes.append(faceoff_line4_left)
+
+goal_line_back_1 = dict(
             type='line',
             xref='x',
             yref='y',
@@ -239,9 +429,23 @@ goal_line1 = dict(
                 width=1
             )
 )
+rink_shapes.append(goal_line_back_1)
 
+goal_line_back_2 = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-64.7',
+            y0='516.2',
+            x1='-82.3',
+            y1='580',
+            line=dict(
+                width=1
+            )
+)
+rink_shapes.append(goal_line_back_2)
 
-goal_line2 = dict(
+goal_line_front_1 = dict(
             type='line',
             xref='x',
             yref='y',
@@ -253,8 +457,21 @@ goal_line2 = dict(
                 width=1
             )
 )
+rink_shapes.append(goal_line_front_1)
 
-# mirror images of "goal_line1" and "goal_line2" along with the Y-axis
+goal_line_front_2 = dict(
+            type='line',
+            xref='x',
+            yref='y',
+            x0='-23.5',
+            y0='516.2',
+            x1='-23.5',
+            y1='493',
+            line=dict(
+                width=1
+            )
+)
+rink_shapes.append(goal_line_front_2)
 
 goal_arc1 = dict(
             type='path',
@@ -265,7 +482,7 @@ goal_arc1 = dict(
                 width=1,
             )
 )
-
+rink_shapes.append(goal_arc1)
 
 goal_arc2 = dict(
             type='path',
@@ -273,10 +490,10 @@ goal_arc2 = dict(
             yref='y',
             path='M 17.6 516.2 C 15 530, -15 530, -17.6 516.2',
             line=dict(
-                width=1,
+                width=1
             )
 )
-
+rink_shapes.append(goal_arc2)
 
 referee_crease = dict(
             type='path',
@@ -288,9 +505,4 @@ referee_crease = dict(
                 color='rgba(255, 0, 0, 1)'
             )
 )
-
-rink_shape.extend([outer_arc1, outer_line, outer_rect, center_red_line, \
-                    center_blue_spot, end_line, blue_line_shape, red_spot1,  \
-                    red_spot1_circle, parallel_line1, parallel_line2, faceoff_line1, \
-                    faceoff_line2, faceoff_line3, faceoff_line4, goal_arc1, goal_arc2, \
-                    goal_line1, goal_line2, referee_crease])
+rink_shapes.append(referee_crease)
