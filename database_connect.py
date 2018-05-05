27a8382c8704dd060aa6350c216025aa5af66f4a
VanditPatel1 <patel.vandit1@gmail.com>
2018-05-05 17:39:30 -0400

    db connector now supports more queries

--- database_connect.py
+++ database_connect.py
@@ -1,6 +1,7 @@
 from config import DRIVER, SERVER, DATABASE, UID, PWD
 import psycopg2
 from create_tables import player_meta, shot_table
+import pandas as pd
 
 
 class db_connect():
@@ -51,6 +52,51 @@ class db_connect():
         else:
             self.conn.commit()
             cur.close()
+
+    def get_player_id(self, id=None, full=None, first=None, last=None):
+
+        if last:
+            SQL = """SELECT id FROM player_meta WHERE {0} = {1};""".format('last_name', last)
+
+        if full is not None:
+            first, last = full.split(' ')
+            SQL = """SELECT id FROM player_meta WHERE {0} = (%s) and {1} = (%s);""".format('first_name', 'last_name')
+            print (SQL)
+        try:
+            cur = self.conn.cursor()
+            res = cur.execute(SQL, (first, last))
+
+        except Exception as ex:
+            print (ex)
+            self.conn.rollback()
+            raise
+
+        else:
+            res = cur.fetchall()
+            cur.close()
+
+            return res
+
+    def get_shots(self, id):
+
+        SQL = """SELECT x_coord, y_coord, goal, period, shot_type, assist_1, assist_2, game_winning, empty_net FROM shots WHERE player_id = {}""".format(id)
+
+        try:
+            cur = self.conn.cursor()
+            res = cur.execute(SQL)
+
+        except Exception as ex:
+            print (ex)
+            self.conn.rollback()
+            raise
+
+        else:
+            res = cur.fetchall()
+            cur.close()
+
+            df = pd.DataFrame(res, columns=['x', 'y', 'goal', 'period', 'shot', 'a1', 'a2', 'gw', 'empt'])
+            return df
+
 
 
 
